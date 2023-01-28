ff8da6636737cdd51daa2be85b5cbc96a4b261a2
Alan Henrique <alanhipermassa@gmail.com>
2023-01-27 22:47:47 -0300

    Codigo Comentado

--- ytDownloader/ytDownloader.py
+++ ytDownloader/ytDownloader.py
@@ -1,11 +1,22 @@
+# importando a Biblioteca pytube
 from pytube import YouTube
-from sys import argv
+# importando a função argv da Biblioteca sys  
+from sys import argv    
 
-link = argv[1]
-yt = YouTube(link)
+# definindo uma variável pra receber os Links pra download e
+# definindo um objeto YouTube
+link = argv[1]  
+yt = YouTube(link)  
 
+# Fazendo o Terminal mostrar o Nome e a Quantidade de Views
+# do vídeo em questão
 print("Title: ", yt.title)
 print("Views: ", yt.views)
 
+# Baixando o Vídeo na maior resolução possível e salvando na pasta 
+# Downloaded Videos
 yd = yt.streams.get_highest_resolution()
 yd.download('./Downloaded Videos')
+
+# Sinal do Fim do Download
+print("Download Complete")
